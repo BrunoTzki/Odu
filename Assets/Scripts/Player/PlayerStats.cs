9ce76b4edfefafe77b1e2084457904e9c417ed29
Bruno Camenietzki Amorim <brunocamenietzki@eefd.ufrj.br>
2023-05-15 02:24:13 -0300

    Efeito na Barra de Vida

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -30,10 +30,8 @@ public class PlayerStats : MonoBehaviour
 
     public void TakeDamage(int damage)
     {
-        currentAra -= damage;
-        int amount = currentAra - damage;
+        int amount = -damage;
         SetHealth(amount);
-        araBar.SetAra(currentAra);
     }
 
     public void SetHealth(int amount)
@@ -43,12 +41,25 @@ public class PlayerStats : MonoBehaviour
             if (isInvencible)
                 return;
         }
-
         currentAra = Mathf.Clamp(currentAra + amount, 0, maxAra);
         
         Vector3 araFillScale = araFill.rectTransform.localScale;
         araFillScale.x = (float)currentAra / (float)maxAra;
         araFill.rectTransform.localScale = araFillScale;
-        
+        StartCoroutine(DecresingAraEfx(araFillScale));
+    }
+
+    IEnumerator DecresingAraEfx(Vector3 newScale)
+    {
+        yield return new WaitForSeconds(0.25f);
+        Vector3 araEfxScale = araEfx.rectTransform.localScale;
+
+        while (araEfx.transform.localScale.x > newScale.x)
+        {
+            araEfxScale.x -= Time.deltaTime * 0.4f;
+            araEfx.rectTransform.localScale = araEfxScale;
+
+            yield return null;
+        }
     }
 }
